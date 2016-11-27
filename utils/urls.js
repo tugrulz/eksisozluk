34cf06702679910920cd35eb898de72904d7da21
Gökay Gürcan <gokaygurcan@gmail.com>
2016-11-28 01:31:08 +0300

    feature: Create urls module to use later

---
+++ utils/urls.js
@@ -0,0 +1,11 @@
+// eksisozluk/eksisozluk
+
+'use strict';
+
+module.exports = {
+  base: 'https://eksisozluk.com/',
+  entry: 'https://eksisozluk.com/entry/',
+  user: 'https://eksisozluk.com/biri/',
+  q: 'https://eksisozluk.com/?q=',
+  seyler: 'https://seyler.eksisozluk.com/'
+};
