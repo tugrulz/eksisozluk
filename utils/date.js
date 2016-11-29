5058d01166c1cb45ed456950c4f1b6a37eef3bc5
Gökay Gürcan <gokaygurcan@gmail.com>
2016-11-29 23:52:33 +0300

    fix: Add moment.js to fix dates

---
+++ utils/date.js
@@ -0,0 +1,46 @@
+// eksisozluk/eksisozluk
+
+// node packages
+const moment = require('moment');
+
+// vars
+const regexes = [
+  /((\d{2}\.){2}\d{4})/,
+  /((\d{2}\.){2}\d{4}\s\d{2}:\d{2})/,
+  /((\d{2}\.){2}\d{4}\s\d{2}:\d{2}\s~\s\d{2}:\d{2})/,
+  /(((\d{2}\.){2}\d{4}\s\d{2}:\d{2})\s~\s((\d{2}\.){2}\d{4}\s\d{2}:\d{2}))/
+];
+
+module.exports = date => {
+  // localize moment
+  moment.locale('tr');
+
+  const modified = date.indexOf(' ~ ') > 0;
+
+  let level = 0;
+  regexes.forEach(regex => {
+    if (regex.test(date)) {
+      level = regexes.indexOf(regex);
+    }
+  });
+
+  const d = {};
+  if (level === 0) {
+    d.created = moment(date + ' 00:00:00', 'DD.MM.YYYY HH:mm:ss');
+    d.modified = null;
+  }
+  else if (level === 1) {
+    d.created = moment(date + ':00', 'DD.MM.YYYY HH:mm:ss');
+    d.modified = null;
+  }
+  else if (level === 2 && modified) {
+    d.created = moment(date.split(' ~ ')[0] + ':00', 'DD.MM.YYYY HH:mm:ss');
+    d.modified = moment(date.split(' ~ ')[0] + ' ' + date.split(' ~ ')[1] + ':00', 'DD.MM.YYYY HH:mm:ss');
+  }
+  else if (level === 3 && modified) {
+    d.created = moment(date.split(' ~ ')[0] + ':00', 'DD.MM.YYYY HH:mm:ss');
+    d.modified = moment(date.split(' ~ ')[1] + ':00', 'DD.MM.YYYY HH:mm:ss');
+  }
+
+  return d;
+};
