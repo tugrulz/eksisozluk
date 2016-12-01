655078c18f09dde89f1201bcd38152d5887f9095
Gökay Gürcan <gokaygurcan@gmail.com>
2016-12-01 23:48:09 +0300

    fix: Try to get higher Code Climate grade

--- utils/date.js
+++ utils/date.js
@@ -12,9 +12,11 @@ const regexes = [
 ];
 
 module.exports = date => {
-  // localize moment
-  moment.locale('tr');
+  moment.locale('tr'); // localize moment
 
+  const dateFormat = 'DD.MM.YYYY HH:mm:ss';
+  const hours = ' 00:00:00';
+  const seconds = ':00';
   const modified = date.indexOf(' ~ ') > 0;
 
   let level = 0;
@@ -26,20 +28,20 @@ module.exports = date => {
 
   const d = {};
   if (level === 0) {
-    d.created = moment(date + ' 00:00:00', 'DD.MM.YYYY HH:mm:ss').format().toString();
+    d.created = moment(date + hours, dateFormat).format().toString();
     d.modified = null;
   }
   else if (level === 1) {
-    d.created = moment(date + ':00', 'DD.MM.YYYY HH:mm:ss').format().toString();
+    d.created = moment(date + seconds, dateFormat).format().toString();
     d.modified = null;
   }
   else if (level === 2 && modified) {
-    d.created = moment(date.split(' ~ ')[0] + ':00', 'DD.MM.YYYY HH:mm:ss').format().toString();
-    d.modified = moment(date.split(' ~ ')[0] + ' ' + date.split(' ~ ')[1] + ':00', 'DD.MM.YYYY HH:mm:ss').format().toString();
+    d.created = moment(date.split(' ~ ')[0] + seconds, dateFormat).format().toString();
+    d.modified = moment(date.split(' ~ ')[0] + ' ' + date.split(' ~ ')[1] + seconds, dateFormat).format().toString();
   }
   else if (level === 3 && modified) {
-    d.created = moment(date.split(' ~ ')[0] + ':00', 'DD.MM.YYYY HH:mm:ss').format().toString();
-    d.modified = moment(date.split(' ~ ')[1] + ':00', 'DD.MM.YYYY HH:mm:ss').format().toString();
+    d.created = moment(date.split(' ~ ')[0] + seconds, dateFormat).format().toString();
+    d.modified = moment(date.split(' ~ ')[1] + seconds, dateFormat).format().toString();
   }
 
   return d;
